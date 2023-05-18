6c424130e851aa5d2306a7fbea5b6d6f08a3aa14
VeraSlaveholder <bulgakova.vera2003@mail.ru>
2023-05-19 00:49:56 +0300

    connecting the Open API 3.0 to the project

--- src/main/java/com/example/Security_REST/controller/RestApiController.java
+++ src/main/java/com/example/Security_REST/controller/RestApiController.java
@@ -3,8 +3,8 @@ package com.example.Security_REST.controller;
 import com.example.Security_REST.Exception.ExceptionInfo;
 import com.example.Security_REST.Exception.UserUsernameExistException;
 import com.example.Security_REST.model.User;
-import com.example.Security_REST.service.RoleService;
 import com.example.Security_REST.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,15 +22,17 @@ public class RestApiController {
     private final UserService userService;
 
     @Autowired
-    public RestApiController(RoleService roleService, UserService userService) {
+    public RestApiController(UserService userService) {
         this.userService = userService;
     }
 
+    @Operation(summary = "Get all users")
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Create a user")
     @PostMapping("/users")
     public ResponseEntity<ExceptionInfo> createUser(@Valid @RequestBody User user, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -45,24 +47,28 @@ public class RestApiController {
         }
     }
 
+    @Operation(summary = "Delete a user by his id")
     @DeleteMapping("/users/{id}")
     public ResponseEntity<ExceptionInfo> pageDelete(@PathVariable("id") int id) {
         userService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a user by his id")
     @GetMapping("users/{id}")
     public ResponseEntity<User> getUser(@PathVariable("id") int id) {
         User user = userService.getById(id);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a user by his name")
     @GetMapping("/user")
     public ResponseEntity<User> getUserByUsername(Principal principal) {
         User user = userService.findByUsername(principal);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
+    @Operation(summary = "Edit a user by his id")
     @PutMapping("/users/{id}")
     public ResponseEntity<ExceptionInfo> pageEdit(@PathVariable("id") int id,
                                                   @Valid @RequestBody User user,
